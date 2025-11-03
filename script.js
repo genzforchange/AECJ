44f4b10830145438e7773bfda4f23b35d47abe47
Hana Memon <41581959+hanamemon@users.noreply.github.com>
2025-11-03 21:21:09 +0000

    updated creators and pfps

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 
 function toCommas(num) {
-    var int = parseInt(num)
-    return int.toLocaleString("en-US")
+  var int = parseInt(num)
+  return int.toLocaleString("en-US")
 }
 
 function capitalize(name) {
   return name.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
-} 
+}
 
 function toMils(num) {
   var str = num.toString()
@@ -24,7 +24,7 @@ function toMils(num) {
   return final
 }
 
-$( document ).ready(function() {
+$(document).ready(function() {
   d3.csv("Creators.csv").then(data => {
     console.log(data)
     d3.shuffle(data)
@@ -37,29 +37,29 @@ $( document ).ready(function() {
     data.forEach(d => {
       $(".marquee-content").append(`<div class="marquee-item"><img src="${d.ProfilePicture}"/><h6>@${d.Handle}<h6><p>${toCommas(d.Following)}</p></div>`)
     })
-    $(".following").text(toMils(follows)) 
+    $(".following").text(toMils(follows))
     var items = $('.signs > li').get();
-    items.sort(function(a,b){
+    items.sort(function(a, b) {
       var keyA = $(a).text();
       var keyB = $(b).text();
-    
+
       if (keyA < keyB) return -1;
       if (keyA > keyB) return 1;
       return 0;
     });
     var ul = $('.signs');
-    $.each(items, function(i, li){
+    $.each(items, function(i, li) {
       ul.append(li); /* This removes li from the old spot and moves it */
     });
   })
-  
+
 })
 // header: when you scroll down, it becomes fixed:
 //https://www.w3schools.com/howto/howto_js_sticky_header.asp
-$(window).scroll(function(){
-      if ($(this).scrollTop() > 120) {
-          $('.header').addClass('fixed');
-      } else {
-          $('.header').removeClass('fixed');
-      }
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 120) {
+    $('.header').addClass('fixed');
+  } else {
+    $('.header').removeClass('fixed');
+  }
 });
